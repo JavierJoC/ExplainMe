888bbe97f7f55940c66cce312bd201955fb94c79
Francisco Javier José Angeles <bocho_zic@gmail.com>
2026-01-12 20:52:08 -0600

    Corrección de una ruta absoluta que impedía que corriera en la pc de un usuario

--- lib/explainme.rb
+++ lib/explainme.rb
@@ -3,7 +3,14 @@ module Explainme
   def self.run
     clipboard="straigfordward"
     explainer="The meaning of #{clipboard}"
-    tts="/home/bocho/AdD/Mec/Xprn/Ars/Techne/ExplainMe/input_prompt.wav"
+
+    ruta_padre = File.join(__dir__, "..", "input_prompt.wav")
+    ruta_completa = File.expand_path(ruta_padre)
+    puts "Ruta relativa: #{ruta_padre}"
+    puts "Ruta absoluta: #{ruta_completa}"
+    tts=ruta_completa
+    puts tts
+
 
     puts explainer
     system("aplay", tts)

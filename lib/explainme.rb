e7f29ec87996b9e3e8f48fc6546fc45775a9cc8b
Francisco Javier José Angeles <bocho_zic@gmail.com>
2026-01-12 23:25:26 -0600

    Integracion del modulo Use_Case, exitoso en local

--- lib/explainme.rb
+++ lib/explainme.rb
@@ -1,21 +1,22 @@
+require_relative "use_case"
 
 
 module Explainme
   def self.run
-    clipboard="straigfordward"
-    explainer="The meaning of #{clipboard}"
+    clipboard   = "straigfordward"
+    explainer   = "The meaning of "
 
     ruta_padre = File.join(__dir__, "..", "input_prompt.wav")
     ruta_completa = File.expand_path(ruta_padre)
-    puts "Ruta relativa: #{ruta_padre}"
-    puts "Ruta absoluta: #{ruta_completa}"
-    tts=ruta_completa
-    puts tts
+    #puts "Ruta absoluta: #{ruta_completa}"
+    tts         = ruta_completa
+    #puts tts
 
-
-    puts explainer
-
-    system("paplay", tts)
+    UseCase.new(
+      clipboard:clipboard,
+      explainer:explainer,
+      tts:tts
+    ).run
 
   end
 end
